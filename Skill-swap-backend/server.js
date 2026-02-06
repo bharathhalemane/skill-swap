22415bfefbedeaf49376c29a9f04af57e9e88adb
bharath <bharathhalemane143@gmail.com>
2026-02-06 14:24:55 +0530

    /

--- Skill-swap-backend/server.js
+++ Skill-swap-backend/server.js
@@ -1,16 +1,28 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const connectDB = require("./config/db")
-const cors = require("cors")
-const passport = require("passport")
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+import cors from "cors"
+import passport from "passport"
 
-dotenv.config()
+//for production purpose
+import path from "path"
 
-require("./config/passport")
+dotenv.config({
+  path: path.join(__dirname, ".env"),
+});
+console.log("ENV CHECK:", {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+});
+import "./config/passport.js"
 
 connectDB()
 
 const app = express()
+
+//for production purpose
+const __dirname = path.resolve();
+console.log(__dirname)
 
 app.use(cors())
 app.use(express.json())        
@@ -18,6 +30,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
 
 app.use("/api/auth", require("./routes/authRoutes"))
+
+app.use(express.static(path.join(__dirname, "/skill-swap-frontend/dist")))
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "skill-swap-frontend", "dist", "index.html"))
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
