dd99823c355e8e6e4401ce4a76326759bcbba593
bharath <bharathhalemane143@gmail.com>
2026-03-01 00:23:15 +0530

    auth error solved

--- Skill-swap-backend/routes/user.js
+++ Skill-swap-backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router() 
-const auth = require("../middleWare/auth")
+const auth = require("../middleware/auth")
 const { updateProfile } = require("../controllers/userController")
 const upload = require("../middleware/upload")
 
