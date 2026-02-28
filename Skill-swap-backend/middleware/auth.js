ba43586cc769bb5f8a32ec0892df81bb60260545
bharath <bharathhalemane143@gmail.com>
2026-03-01 00:20:09 +0530

    auth error solved

--- Skill-swap-backend/middleware/auth.js
+++ Skill-swap-backend/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken")
 
-module.exports = (req, res, next) => {
+const auth = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1]
 
     if (!token) { return res.status(401).json({ message: "Unauthorized" }) }
@@ -13,3 +13,5 @@ module.exports = (req, res, next) => {
         res.status(401).json({message: "Invalid token"})
     }
 }
+
+module.exports = auth
