6636135511ad300b70667188cb91ea8b318194f8
bharath <bharathhalemane143@gmail.com>
2026-03-03 03:03:39 +0530

    msg error solving

--- Skill-swap-backend/middleware/auth.js
+++ Skill-swap-backend/middleware/auth.js
@@ -1,16 +1,21 @@
 const jwt = require("jsonwebtoken")
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1]
 
     if (!token) { return res.status(401).json({ message: "Unauthorized" }) }
-    
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = decoded 
+        const user = await User.findById(decoded.id);
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = user;
         next()
     } catch (err) {
-        res.status(401).json({message: "Invalid token"})
+        res.status(401).json({ message: "Invalid token" })
     }
 }
 
