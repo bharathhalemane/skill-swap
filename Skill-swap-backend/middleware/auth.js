eb1f2a00944a2928ac1c0b0c4dac40ab940114cd
bharath <bharathhalemane143@gmail.com>
2026-03-03 03:17:30 +0530

    msg error solving

--- Skill-swap-backend/middleware/auth.js
+++ Skill-swap-backend/middleware/auth.js
@@ -8,7 +8,7 @@ const auth = async (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findById(decoded.id);
+        const user = await User.findById(decoded._id);
         if (!user) {
             return res.status(401).json({ message: "User not found" });
         }
