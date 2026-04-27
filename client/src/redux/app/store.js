be3957655d73889a1526739fc218095dec5d483e
bharath <bharathhalemane143@gmail.com>
2026-04-27 22:51:05 +0530

    Update Redux store configuration

--- client/src/redux/app/store.js
+++ client/src/redux/app/store.js
@@ -2,10 +2,12 @@ import {configureStore} from "@reduxjs/toolkit"
 
 import skillsReducer from "../features/skills/skillsSlice"
 import comSkillsReducer from "../features/completedSkills/comSkillsSlice"
+import profileReducer from "../features/profile/ProfileSlice"
 const store = configureStore({
     reducer: {
         skills: skillsReducer,
-        comSkills: comSkillsReducer
+        comSkills: comSkillsReducer,
+        profile: profileReducer
     }
 })
 
