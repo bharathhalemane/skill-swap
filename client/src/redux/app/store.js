bf5ba819d444efd87678486ffdb5d4da4581632f
bharath <bharathhalemane143@gmail.com>
2026-04-29 22:26:43 +0530

    updated profile store

--- client/src/redux/app/store.js
+++ client/src/redux/app/store.js
@@ -7,7 +7,7 @@ const store = configureStore({
     reducer: {
         skills: skillsReducer,
         comSkills: comSkillsReducer,
-        profile: profileReducer
+        profile: profileReducer,
     }
 })
 
