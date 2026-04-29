241cfe9b363e9759fd1c9be1c8481113b0ff22a9
bharath <bharathhalemane143@gmail.com>
2026-04-29 22:27:19 +0530

    updated actions in fetching the profile data

--- client/src/redux/features/profile/ProfileActions.js
+++ client/src/redux/features/profile/ProfileActions.js
@@ -1,15 +1,31 @@
 import axios from "axios"
-import { fetchProfile } from "./ProfileSlice"
+import { fetchProfile, fetchProfileStart, fetchProfileFailure } from "./ProfileSlice"
 import Cookies from "js-cookie"
 
 export const fetchProfileData = () => async (dispatch, getState) => {
-    const userId = Cookies.get("userId")
-    const token = Cookies.get("jwtToken")
-    const url = `${import.meta.env.VITE_PROFILE_API}/${userId}`
-    const res = await axios.get(url, {
-        headers: { Authorization: `Bearer ${token}` }
-    })
-    const profile = res.data.user.profile
-    dispatch(fetchProfile(profile))
+    try {
+        dispatch(fetchProfileStart())
+        const userId = Cookies.get("userId")
+        const token = Cookies.get("jwtToken")
+        const url = `${import.meta.env.VITE_PROFILE_API}/${userId}`
+        const response = await axios.get(url, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        const data = response.data.user
+        const cData = {
+            email: data.email,
+            name: data.name,
+            profile: {
+                username: data.profile.username,
+                bio: data.profile.bio,
+                location: data.profile.location,
+                profileImage: data.profile.profile_image,
+            },
+            phoneNumber: data.phoneNumber
+        }
+        const profile = cData
+        dispatch(fetchProfile(profile))
+    } catch (err) {
+        dispatch(fetchProfileFailure(err.message))
+    }
 }
-
