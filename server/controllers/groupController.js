239de12c8cbedbaeb3228ee94f14bc7e5877a94a
bharath <bharathhalemane143@gmail.com>
2026-05-07 21:07:29 +0530

    accept reject getrequest apis are created

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -34,7 +34,7 @@ exports.createGroup = async (req, res) => {
         res.status(201).json({
             success: true,
             message: "Group created Successfully",
-            data : group,
+            data: group,
         })
     } catch (err) {
         res.status(500).json({ message: "Internal server error!" })
@@ -62,5 +62,197 @@ exports.getAllGroups = async (req, res) => {
     }
 }
 
+exports.searchGroups = async (req, res) => {
+    try {
+        const { query } = req.query
+        const groups = await Group.find({
+            $or: [
+                {
+                    title: {
+                        $regex: query,
+                        $option: "i"
+                    }
+                },
+                {
+                    description: {
+                        $regex: query,
+                        $options: "i"
+                    }
+                }
+            ]
+        })
+            .populate("host", "name profile")
+            .sort({ createdAt: -1 });
+        const updatedGroups = groups.map(group => ({
+            ...group.toObject(),
+            membersCount: group.members.length
+        }));
+
+        res.status(200).json({
+            data: updatedGroups
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
 
 
+exports.sendJoinRequest = async (req, res) => {
+    try {
+        const groupId = req.params.id
+        const userId = req.user.userId
+
+        const group = await Group.findById(groupId)
+
+        if (!groupId) {
+            return res.status(404).json({
+                message: "Group not found"
+            })
+        }
+
+        const alreadyMember = group.members.some(
+            member => member.toString() === userId
+        )
+
+        if (alreadyMember) {
+            return res.status(400).json({
+                message: "Already a member"
+            })
+        }
+
+        const alreadyRequested = group.joinRequests.some(
+            requests => request.toString() === userId
+        )
+
+        if (alreadyRequested) {
+            return res.status(400).json({
+                message: "Request already sent"
+            })
+        }
+
+        if (group.members.length >= group.maxMembers) {
+            return res.status(400).json({
+                message: "Group is full"
+            })
+        }
+
+        group.joinRequests.push(userId)
+
+        await group.save()
+
+        res.status(200).json({
+            message: "join request sent successfully"
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+exports.acceptJoinRequest = async (req, res) => {
+    try {
+        const { id, userId } = req.params
+        const group = await Group.findById(id)
+
+        if (!group) {
+            return res.status(404).json({
+                message: "Group not found"
+            })
+        }
+
+        if (group.host.toString() !== req.user.userId) {
+            return res.status(403).json({
+                message: "Access denied"
+            })
+        }
+
+        if (group.members.length >= group.maxMembers) {
+            return res.status(400).json({
+                message: "Group is full"
+            })
+        }
+
+        group.joinRequests = group.joinRequests.filter(
+            request => request.toString() !== userId
+        )
+
+        group.members.push(userId)
+
+        await group.save()
+
+        res.status(200).json({
+            message: "Request accepted"
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+exports.rejectJoinRequest = async (req, res) => {
+    try {
+        const { id, userId } = req.params
+        const group = await Group.findById(id)
+
+        if (!group) {
+            return res.status(404).json({
+                message: "Group not found"
+            })
+        }
+
+        if (group.host.toString() !== req.user.userId) {
+            return res.status(403).json({
+                message: "Access Denied"
+            })
+        }
+
+        group.joinRequests = group.joinRequests.filter(
+            requests => request.toString() !== userId
+        )
+
+        await group.save()
+
+        res.status(200).json({
+            message: "Request rejected"
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+
+exports.getJoinRequests = async (req, res) => {
+    try {
+        const groupId = req.params.id
+        const userId = req.user.userId
+
+        const group = await Group.findById(groupId)
+            .populate("joinRequests", "name profile")
+
+        if (!group) {
+            return res.status(404).json({
+                message: "Group not found"
+            })
+        }
+
+        if (group.host.toString() !== userId) {
+            return res.status(403).json({
+                message: "Access denied"
+            })
+        }
+
+        res.status(200).json({
+            data: group.joinRequests
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
